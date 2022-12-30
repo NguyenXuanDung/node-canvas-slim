be2b16c32f7660965fcf9beac6cb862d28790d1c
CanIGetaPR <45294513+CanIGetaPR@users.noreply.github.com>
2022-12-29 23:12:15 -0500

    Remove svg and pdf backends

--- src/Backends.cc
+++ src/Backends.cc
@@ -1,8 +1,6 @@
 #include "Backends.h"
 
 #include "backend/ImageBackend.h"
-#include "backend/PdfBackend.h"
-#include "backend/SvgBackend.h"
 
 using namespace v8;
 
@@ -11,8 +9,6 @@ void Backends::Initialize(Local<Object> target) {
 
   Local<Object> obj = Nan::New<Object>();
   ImageBackend::Initialize(obj);
-  PdfBackend::Initialize(obj);
-  SvgBackend::Initialize(obj);
 
   Nan::Set(target, Nan::New<String>("Backends").ToLocalChecked(), obj).Check();
 }
